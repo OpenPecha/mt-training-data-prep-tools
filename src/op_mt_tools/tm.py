0c856ffa18be15004d963b7e34329f12c820e3e4
pechawa <openpecha-bot@openpecha.org>
2023-04-21 17:51:29 +0530

    feat: run aligner with gradio client

--- src/op_mt_tools/tm.py
+++ src/op_mt_tools/tm.py
@@ -1,9 +1,37 @@
+import json
+import logging
+import tempfile
+import time
 from pathlib import Path
-from typing import Dict, Optional
+from typing import Dict
 
-import requests
+from gradio_client import Client
+from gradio_client.utils import Status as JobStatus
 
 from . import types as t
+
+client = None
+
+
+def get_client():
+    global client
+    if not client:
+        client = Client("openpecha/tibetan-aligner-api")
+    return client
+
+
+def run_aligner(input_json_fn: Path):
+    client = get_client()
+    job = client.submit(
+        input_json_fn,
+        api_name="/align",
+    )
+
+    logging.info("Waiting for Alignment Job to start...")
+    while job.status().code != JobStatus.PROCESSING:
+        time.sleep(1)
+    logging.info("Alignment Job started")
+    return "PROCESSING"
 
 
 def get_raw_github_file_url(local_view_path: Path):
@@ -23,21 +51,31 @@ def get_raw_github_file_url(local_view_path: Path):
     )
 
 
-def create_TM(
-    text_pair_view_path: Dict[t.LANG_CODE, Path], text_id: str
-) -> Optional[dict]:
-    def create_request_body(text_id, text_pair_view_path):
-        request_body = {
-            "text_id": text_id,
-        }
-        for lang_code, view_path in text_pair_view_path.items():
-            request_body[f"{lang_code}_file_url"] = get_raw_github_file_url(view_path)
+def create_request_body(text_id, text_pair_view_path, base_dir: Path) -> Path:
+    """Create request body for TM creation.
 
-    request_body = create_request_body(text_id, text_pair_view_path)
-    r = requests.post("https://api.monlam.ai/mt/create-tm", json=request_body)
+    Args:
+        text_id: Text id.
+        text_pair_view_path: Dict of language code and view path.
 
-    if r.status_code != requests.codes.ok:
-        print(f"[ERROR] Failed to algin {text_id}. Error code {r.status_code}")
-        return None
-    else:
-        return r.json()["data"]
+    Returns:
+        request_json_fn(Path): Request body saved in json.
+    """
+    request_body = {
+        "text_id": text_id,
+    }
+    for lang_code, view_path in text_pair_view_path.items():
+        request_body[f"{lang_code}_file_url"] = get_raw_github_file_url(view_path)
+
+    request_body_json_fn = base_dir / "request_body.json"
+    json.dump(request_body, request_body_json_fn.open("w"))
+    return request_body_json_fn
+
+
+def create_TM(text_pair_view_path: Dict[t.LANG_CODE, Path], text_id: str) -> str:
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        request_body_json_fn = create_request_body(
+            text_id, text_pair_view_path, base_dir=Path(tmp_dir)
+        )
+        status = run_aligner(request_body_json_fn)
+        return status
