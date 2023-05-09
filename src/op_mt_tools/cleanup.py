76fe1c38bf47273d59f1a6fb5a50785ae7d1417d
test <ten13zin@gmail.com>
2023-05-09 18:07:23 +0530

    fix(cleanup): use tiktoken to find prompt tokens

--- src/op_mt_tools/cleanup.py
+++ src/op_mt_tools/cleanup.py
@@ -1,31 +1,88 @@
 import os
 import re
-from collections.abc import Generator
 from pathlib import Path
 from typing import List
 
 import openai
+import tiktoken
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+OPENAI_MODEL = "gpt-3.5-turbo-0301"
 
 
-def split_document(document, max_tokens=4096) -> Generator[str, None, None]:
+def num_tokens_from_messages(text, model=OPENAI_MODEL):
+    """Returns the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+
+    if model == "gpt-3.5-turbo":
+        print(
+            "Warning: gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo-0301."
+        )
+        return num_tokens_from_messages(text, model="gpt-3.5-turbo-0301")
+    elif model == "gpt-4":
+        print(
+            "Warning: gpt-4 may change over time. Returning num tokens assuming gpt-4-0314."
+        )
+        return num_tokens_from_messages(text, model="gpt-4-0314")
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = (
+            4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        )
+    elif model == "gpt-4-0314":
+        tokens_per_message = 3
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. \
+                See https://github.com/openai/openai-python/blob/main/chatml.md \
+                for information on how messages are converted to tokens."""
+        )
+    return len(encoding.encode(text)) + tokens_per_message
+
+
+CLEANUP_PROMPT = """
+Perform the following actions:
+1 - Split the text delimited by triple backtickcs into sentences.
+2 - Join sentences that are split across lines.
+3 - Delete page numbers.
+4 - Never change the text unless it's a spelling mistake.
+5 - Output each sentence on a new line.
+
+Text:
+```{}```
+"""
+
+
+def split_document(
+    document: str, prompt_template: str, max_tokens: int = 4097
+) -> List[str]:
+    """Splits a document into chunks of text that are less than max_tokens long."""
+    prompt_template_tokens = num_tokens_from_messages(prompt_template.format(""))
+
+    chunks = []
+    current_chunk = []
     sentences = re.split("(?<=[.!?]) +", document)
-    chunk = []
-
     for sentence in sentences:
-        chunk.append(sentence)
+        current_chunk.append(sentence)
         # Check if the current chunk has more tokens than the limit
-        if len(" ".join(chunk).split()) > max_tokens:
+        tokens = num_tokens_from_messages(" ".join(current_chunk))
+        if tokens + prompt_template_tokens > max_tokens:
             # If it exceeds the limit, remove the last added sentence and store the chunk
-            chunk.pop()
-            chunk = [sentence]
+            current_chunk.pop()
+            chunks.append(" ".join(current_chunk))
+            current_chunk = [sentence]
 
-        yield " ".join(chunk)
-        chunk = []
+    # Add the last chunk if it's not empty
+    if current_chunk:
+        chunks.append(" ".join(current_chunk))
+
+    return chunks
 
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
+def get_completion(prompt, model=OPENAI_MODEL):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
@@ -36,26 +93,16 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 
 
 def get_sents_with_chatgpt(text: str) -> List[str]:
-    prompt = f"""
-Perform the following actions:
-1 - Split the text delimited by triple backtickcs into sentences.
-2 - Join sentences that are split across lines.
-3 - Delete page numbers.
-4 - Never change the text unless it's a spelling mistake.
-5 - Output each sentence on a new line.
-
-Text:
-```{text}```
-"""
+    prompt = CLEANUP_PROMPT.format(text)
     response = get_completion(prompt)
     return response.split("\n")
 
 
-def cleaning_pipeline(fn: Path) -> Path:
+def run_cleanup(fn: Path) -> Path:
     cleaned_fn = fn.parent / f"[GPT_CLEANED]_{fn.stem}.txt"
     text = fn.read_text(encoding="utf-8")
     with cleaned_fn.open("+a") as cleaned_file:
-        for chunk in split_document(text):
+        for chunk in split_document(text, prompt_template=CLEANUP_PROMPT):
             sents = get_sents_with_chatgpt(chunk)
             cleaned_file.writelines(sents)
     return cleaned_fn
--- tests/test_cleanup.py
+++ tests/test_cleanup.py
@@ -1,9 +1,22 @@
-from op_mt_tools.cleanup import split_document
+from pathlib import Path
+
+import pytest
+
+from op_mt_tools.cleanup import CLEANUP_PROMPT, run_cleanup, split_document
 
 
 def test_get_text_chunks():
-    text = "12. 3456. 7890"
+    text = "Hello World. Hello World"
 
-    chunks = list(split_document(text, max_tokens=3))
+    sents = split_document(text, prompt_template=CLEANUP_PROMPT, max_tokens=75)
 
-    assert chunks == ["12.", "3456.", "7890"]
+    assert sents == ["Hello World.", "Hello World"]
+
+
+@pytest.mark.skip(reason="Need to Mock OpenAI API")
+def test_run_cleanup():
+    fn = Path(__file__).parent / "manual" / "uncleaned_texts" / "01.txt"
+
+    cleaned_fn = run_cleanup(fn)
+
+    assert cleaned_fn.is_file()
