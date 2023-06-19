58f8e063d3d5c6e11330724cd405837e79825240
10zinten <ten13zin@gmail.com>
2023-06-19 13:06:22 +0530

    fix(catalog): parse bo-en catalog

--- src/op_mt_tools/catalog.py
+++ src/op_mt_tools/catalog.py
@@ -7,10 +7,11 @@ from tinydb import TinyDB
 from op_mt_tools import config
 
 catalog_path = config.ROOT_DIR / "data" / "catalog.json"
+catalog_csv_output_path = config.ROOT_DIR / "data" / "catalog.csv"
 
 
 @dataclass
-class Item:
+class TMCatalogItem:
     tm_id: str = ""
     direction: str = ""
     en_id: str = ""
@@ -24,18 +25,18 @@ class Item:
 
     @classmethod
     def from_dict(cls, d):
-        return Item(**d)
+        return TMCatalogItem(**d)
 
     def to_dict(self):
         return asdict(self)
 
 
-class CatalogDB:
+class TMCatalogDB:
     def __init__(self, db_path):
         self._db_path = db_path
         self._db = TinyDB(catalog_path)
 
-    def add_item(self, item: Item):
+    def add_item(self, item: TMCatalogItem):
         self._db.insert(item.to_dict())
 
 
@@ -43,6 +44,14 @@ def read_csv(path):
     with open(path) as file:
         reader = csv.DictReader(file)
         yield from reader
+
+
+def write_dict_to_csv(data, path):
+    fieldnames = data[0].keys() if data else []
+    with open(path, "w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
 
 
 @dataclass
@@ -57,7 +66,7 @@ class TextPair:
 
     @classmethod
     def from_dict(cls, d):
-        return Item(**d)
+        return TextPair(**d)
 
     def to_dict(self):
         return asdict(self)
@@ -66,6 +75,7 @@ class TextPair:
 class ENBOCatalog:
     def __init__(self, path):
         self._path = path
+        self.direction = "enbo"
 
     def get_rows(self):
         for row in read_csv(self._path):
@@ -78,26 +88,101 @@ class ENBOCatalog:
             if row["Clean"] == "TRUE" and row["QCed"] == "✔":
                 yield row
 
+    def _to_text_pair(self, row):
+        return TextPair(
+            direction=self.direction,
+            en_id=row["EN Repo"].strip(),
+            en_title=row["དབྱིན་མིང་། English Title"].strip(),
+            en_repo_url=row["དྲ་ཐག Links (EN)"].strip(),
+            bo_id=row["BO Repo"].strip(),
+            bo_title=row["བོད་མིང་། Tibetan Title"].strip(),
+            bo_repo_url=row["དྲ་ཐག Links (BO)"].strip(),
+        )
+
     def get_cleaned_items(self):
         for row in self.get_cleaned_rows():
-            yield TextPair(
-                direction="enbo",
-                en_id=row["EN Repo"],
-                en_title=row["དབྱིན་མིང་། English Title"],
-                en_repo_url=row["དྲ་ཐག Links (EN)"],
-                bo_id=row["BO Repo"],
-                bo_title=row["བོད་མིང་། Tibetan Title"],
-                bo_repo_url=row["དྲ་ཐག Links (BO)"],
-            )
+            yield self._to_text_pair(row)
+
+    def get_items(self):
+        for row in self.get_rows():
+            yield self._to_text_pair(row)
+
+
+class BOENCatalog:
+    def __init__(self, path):
+        self._path = path
+        self.direction = "boen"
+
+    def get_rows(self):
+        for row in read_csv(self._path):
+            if not row["Tittle"] or row["Tittle"].isdigit():
+                continue
+            yield row
+
+    def get_cleaned_rows(self):
+        for row in self.get_rows():
+            if row["Done"] == "TRUE" and row["QCed"] == "✔":
+                yield row
+
+    def _to_text_pair(self, row):
+        return TextPair(
+            direction=self.direction,
+            en_id=row["EN Repo"].strip(),
+            en_title=row["Tittle"].strip(),
+            en_repo_url=row["Link"].strip(),
+            bo_id=row["BO Repo"].strip(),
+            bo_title=row["Bo title-0"].strip(),
+            bo_repo_url=row["Link-0"].strip(),
+        )
+
+    def get_cleaned_items(self):
+        for row in self.get_cleaned_rows():
+            yield self._to_text_pair(row)
+
+    def get_items(self):
+        for row in self.get_rows():
+            yield self._to_text_pair(row)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--enbo_catalog", help="path to enbo catalog csv file")
-    parser.add_argument("--boen_catalog", help="path to boen catalog csv file")
+    parser.add_argument("enbo_path", help="path to enbo catalog csv file")
+    parser.add_argument("boen_path", help="path to boen catalog csv file")
+    parser.add_argument(
+        "--only_cleaned", help="only cleaned items", action="store_true"
+    )
+    parser.add_argument(
+        "--output_path",
+        help="path to output catalog json file",
+        default=catalog_csv_output_path,
+    )
 
     args = parser.parse_args()
+    enbo_catalog = ENBOCatalog(args.enbo_path)
+    boen_catalog = BOENCatalog(args.boen_path)
+    catalog_db = TMCatalogDB(catalog_path)
 
-    enbo_catalog = ENBOCatalog(args.enbo_catalog)
-    for cleaned_item in enbo_catalog.get_cleaned_items():
-        print(cleaned_item)
+    if args.only_cleaned:
+        texts_pairs = list(enbo_catalog.get_cleaned_items()) + list(
+            boen_catalog.get_cleaned_items()
+        )
+    else:
+        print("no cleaned")
+        texts_pairs = list(enbo_catalog.get_items()) + list(boen_catalog.get_items())
+    tm_catalog_items = []
+    for text_pair in enbo_catalog.get_cleaned_items():
+        tm_catalog_item = TMCatalogItem(
+            tm_id=f"TM{text_pair.en_id[2:]}",
+            direction=text_pair.direction,
+            en_id=text_pair.en_id,
+            en_title=text_pair.en_title,
+            en_repo_url=text_pair.en_repo_url,
+            bo_id=text_pair.bo_id,
+            bo_title=text_pair.bo_title,
+            bo_repo_url=text_pair.bo_repo_url,
+        )
+
+        tm_catalog_items.append(tm_catalog_item.to_dict())
+        # catalog_db.add_item(tm_catalog_item)
+
+    write_dict_to_csv(tm_catalog_items, args.output_path)
