4b599be007485f9d86e21e77c7575fe3a08aac5f
test <ten13zin@gmail.com>
2023-08-08 17:18:56 +0530

    feat: add TM qc pipeline

---
+++ src/op_mt_tools/qc/tm.py
@@ -0,0 +1,73 @@
+import argparse
+import os
+
+import requests
+
+qc_failed_tm_ids_fn = "TM_failed_qc.txt"
+
+
+def get_github_file_contents(owner, repo, access_token, file_extension=".txt"):
+    url = f"https://api.github.com/repos/{owner}/{repo}/contents"
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    files = response.json()
+    text_files = [
+        file
+        for file in files
+        if file["type"] == "file" and file["name"].endswith(file_extension)
+    ]
+    return text_files
+
+
+def check_text_file_lines(owner, repo, access_token):
+    text_files = get_github_file_contents(owner, repo, access_token)
+
+    for file in text_files:
+        url = file["download_url"]
+        response = requests.get(
+            url, headers={"Authorization": f"Bearer {access_token}"}
+        )
+        response.raise_for_status()
+
+        content = response.text
+        lines = content.strip().split("\n")
+
+        if len(lines) > 1:
+            print(f"File: {file['name']} has more than one line.")
+        else:
+            print(f"File: {file['name']} contains only one line.")
+
+
+def log_failed_qc_tm_id(tm_id: str):
+    with open(qc_failed_tm_ids_fn, "a") as f:
+        f.write(tm_id + "\n")
+
+
+def qc_pipeline(tm_id: str):
+    owner = "MonlamAI"
+    access_token = os.environ["GITHUB_TOKEN"]
+
+    if not check_text_file_lines(owner, tm_id, access_token):
+        log_failed_qc_tm_id(tm_id)
+
+
+def run_qc(args):
+    for tm_id in args.tm_ids:
+        qc_pipeline(tm_id)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Check QC on TMs")
+
+    parser.add_argument(
+        "tm_ids",
+        nargs="+",
+        help="list of tm ids to QC",
+    )
+
+    args = parser.parse_args()
+
+    run_qc(args)
