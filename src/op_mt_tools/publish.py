04e5f9539cc25d9e0ad989481194bba16efbaaa0
10zinten <ten13zin@gmail.com>
2023-08-24 10:56:22 +0530

    fix(publish): add list of TMs to the dataset

--- src/op_mt_tools/publish.py
+++ src/op_mt_tools/publish.py
@@ -1,67 +1,50 @@
 import argparse
-import datetime
+import logging
 import os
-import random
-import string
 import subprocess
-import time
 from pathlib import Path
+from typing import List
+
+from .logger import setup_logger
+
+log_fn = setup_logger(__name__)
+
+GITHUB_ORG = os.environ["MAI_GITHUB_ORG"]
 
 
 def commit_and_push_changes(repo_path, msg):
     # Commit and push submodule changes
     os.chdir(repo_path)
-    print("[INFO] Committing and pushing changes...")
     subprocess.run(["git", "add", "--all"])
     subprocess.run(["git", "commit", "-m", msg])
     subprocess.run(["git", "push"])
 
 
-def get_TMs_ids(repo_path: Path):
-    """Get all latest TMs."""
-    os.chdir(repo_path)
-    subprocess.run(["git", "pull"])
-    for path in Path(repo_path).iterdir():
-        if path.name == "README.md":
-            continue
-        yield path.name
+def get_tm_github_url(tm_id: str):
+    tm_url = f"https://github.com/{GITHUB_ORG}/{tm_id}.git"
+    return tm_url
 
 
-def export_TM(tm: str, export_dir: Path, branch, parent_dir):
+def add_submodule(submodule_path: str, submodule_url: str, repo_path: str):
     """Export TM as submodules of `output_dir`."""
-    tm_url = f"https://github.com/{os.environ['MAI_GITHUB_ORG']}/{tm}.git"
-    submodule_path = f"{parent_dir}/{tm}"
-    subprocess.run(
-        ["git", "submodule", "add", "-b", branch, "--force", tm_url, submodule_path],
-        cwd=export_dir,
+    result = subprocess.run(
+        [
+            "git",
+            "submodule",
+            "add",
+            "-b",
+            "main",
+            "--force",
+            submodule_url,
+            submodule_path,
+        ],
+        cwd=repo_path,
+        capture_output=True,
+        text=True,
     )
 
-
-def add_TMs(
-    tms_repo_path: Path,
-    publish_todo_repo_path: Path,
-    branch: str = "main",
-    parent_dir: str = "data",
-):
-    """Export all latest TMs."""
-    print("[INFO] Adding New TMs ...")
-    tm_ids = get_TMs_ids(publish_todo_repo_path)
-
-    if not tm_ids:
-        print("[INFO] No TM found. Exiting...")
-        return
-
-    for tm_id in tm_ids:
-        if not tm_id:
-            continue
-        if tms_repo_path.joinpath(tm_id).exists():
-            print(f"[INFO] {tm_id} already exists. Skipping...")
-            continue
-        export_TM(tm_id, tms_repo_path, branch, parent_dir)
-
-    msg = "add TMs on " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    commit_and_push_changes(tms_repo_path, msg)
-    print("[INFO] Exporting all TMs done!")
+    if result.returncode != 0:
+        raise RuntimeError(result.stderr)
 
 
 def update_submodules(repo_path):
@@ -73,74 +56,66 @@ def update_submodules(repo_path):
     subprocess.run(["git", "submodule", "update", "--init", "--remote", "--recursive"])
 
 
-def create_release(version):
-    # Create new release with tag of current date
-    print("[INFO] Creating new release...")
-    release_title = f"Release {version}"
-    subprocess.run(
-        [
-            "gh",
-            "release",
-            "create",
-            f"{version}",
-            "--title",
-            release_title,
-            "--notes",
-            f"Please provide your feekback on this new release in the issue titled `{version} Feedback`",  # noqa
-        ]
-    )
+def add_TM(
+    dataset_path: Path,
+    tm_id: str,
+    tms_path: str = "data",
+):
+    """Add TM to dataset."""
+    dataset_path = Path(dataset_path).resolve()
+    tm_path = dataset_path / tms_path / tm_id
+    if tm_path.exists():
+        logging.debug(f"TM: {tm_id} already exists in {dataset_path.name}")
+        return
+
+    try:
+        tm_github_url = get_tm_github_url(tm_id)
+        tm_relative_path = f"{tms_path}/{tm_id}"
+        add_submodule(tm_relative_path, tm_github_url, str(dataset_path))
+    except Exception as e:
+        logging.error(f"{tm_id} Failed to add to {dataset_path.name}: {e}")
+
+        return
+
+    logging.info(f"{tm_id} added to {dataset_path.name}")
 
 
-def open_feedback_issue(version):
-    # Open issue for users
-    issue_title = f"{version} Feedback"
-    issue_body = f"Please provide your feedback on the release {version}. We would love to hear from you!"
-    subprocess.run(
-        [
-            "gh",
-            "issue",
-            "create",
-            "--title",
-            issue_title,
-            "--body",
-            issue_body,
-            "--label",
-            "feedback",
-        ],
-    )
+def add_tms_to_dataset(dataset_path: Path, tm_ids: List[str]):
+    for tm_id in tm_ids:
+        add_TM(dataset_path, tm_id)
+    commit_and_push_changes(dataset_path, "Add TMs")
 
 
-def generate_release_tag():
-    # Generate release tag with random two-character alphabets
-    current_date = datetime.date.today().strftime("%d.%m.%Y")
-    random_chars = "".join(random.choices(string.ascii_lowercase, k=2))
-    release_tag = f"{current_date}-{random_chars}"
-    return release_tag
+def print_success_tms():
+    for line in log_fn.read_text().splitlines():
+        if "- INFO -" in line:
+            print(line.split(" ")[0])
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="Update submodules, create release, and open issue for a repository"
-    )
-    parser.add_argument("tms_repo_path", help="TMs repo path")
-    parser.add_argument("--publish_todo_path", help="Publish TODO repo path")
-    parser.add_argument(
-        "--no-release", action="store_true", help="Skip creating a new release"
-    )
-    args = parser.parse_args()
-
-    # Update submodules
-    if args.publish_todo_path:
-        add_TMs(Path(args.tms_repo_path), Path(args.publish_todo_path))
-    update_submodules(args.tms_repo_path)
-    commit_and_push_changes(args.tms_repo_path, "update TMs")
-
-    # Create new release or skip if specified
-    if not args.no_release:
-        release_tag = generate_release_tag()
-        create_release(release_tag)
-        open_feedback_issue(release_tag)
+def print_failed_tms():
+    for line in log_fn.read_text().splitlines():
+        if "- ERROR -" in line:
+            print(line.split(" ")[0])
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Update submodules, create release, and open issue for a repository"
+    )
+    parser.add_argument("dataset_path", help="TMs repo path")
+    parser.add_argument(
+        "tm_ids",
+        nargs="+",
+        help="TM ids to be added",
+    )
+    parser.add_argument(
+        "--update_tms",
+        action="store_true",
+        help="whether to update tms",
+    )
+    args = parser.parse_args()
+
+    add_tms_to_dataset(Path(args.dataset_path), args.tm_ids)
+
+    if args.update_tms:
+        update_submodules(args.dataset_path)
